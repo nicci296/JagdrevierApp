c4bb5bd9164799d0df1e79a32a1e43ea4b927945
m3k1 <mi.schneider.90@googlemail.com>
2020-08-12 14:50:45 +0200

    Revierverwaltung Activity als EmptyActivity angemeldet

---
+++ app/src/main/java/com/example/jagdrevierapp/Revierverwaltung.java
@@ -0,0 +1,14 @@
+package com.example.jagdrevierapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Revierverwaltung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revierverwaltung);
+    }
+}
