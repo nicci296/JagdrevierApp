51c22f1180d794aeb86e019eae04e614332ae9a9
StudentM3k1 <michael.schneider2@iubh-fernstudium.de>
2020-08-15 15:12:43 +0200

    Login funktioniert. Verbindung Database klappt, Auslesen Usesr vorhanden klappt. Logged in funktioniert
    Logout in Jagdeinrichtungen funktioniert.
    
    Nächste Schritte:
    - leere Textfelder behandlen (führt zu Appabsturz)
    - Navigation einrichten

--- app/src/main/java/com/example/jagdrevierapp/JagdeinrichtungenVerwalten.java
+++ app/src/main/java/com/example/jagdrevierapp/JagdeinrichtungenVerwalten.java
@@ -1,13 +1,72 @@
 package com.example.jagdrevierapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class JagdeinrichtungenVerwalten extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class JagdeinrichtungenVerwalten extends AppCompatActivity implements View.OnClickListener{
+
+    //Const variables
+    public static final String ANONYMOUS = "anonymous";
+
+    //general variables
+    private String mUsername;
+    private GoogleSignInClient mSignInClient;
+
+    //Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jagdeinrichtungen_verwalten);
+
+        //onClickListener für Buttons
+        Button logoutBtn = findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(this);
+
+        //Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            //Nicht eingeloggt, SignIn-Activity wird gestartet
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        } else {
+            mUsername =mFirebaseUser.getDisplayName();
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logoutBtn:
+                mFirebaseAuth.signOut();
+                mSignInClient.signOut();
+
+                mUsername = ANONYMOUS;
+                startActivity(new Intent(this, LoginActivity.class));
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.logoutBtn) {
+            mFirebaseAuth.signOut();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
     }
 }
--- app/src/main/java/com/example/jagdrevierapp/LoginActivity.java
+++ app/src/main/java/com/example/jagdrevierapp/LoginActivity.java
@@ -3,9 +3,12 @@ package com.example.jagdrevierapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.icu.text.IDNA;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,59 +17,143 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+public class LoginActivity extends AppCompatActivity implements
+    View.OnClickListener {
+
+    private static final String TAG = "EmailPassword";
+
+    //Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+        //Buttons
+        Button logInBtn = findViewById(R.id.LoginBtn);
+        logInBtn.setOnClickListener(this);
+        Button registBtn = findViewById(R.id.registBtn);
+        registBtn.setOnClickListener(this);
 
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
+    //Check if user is already signed in
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
         updateUI(currentUser);
     }
 
-    private void singIn(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    //Log.d(INFO, "signInWithEmail:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    updateUI(user);
-                } else {
-                    // If sign in fails, display a message to the user.
-                    //Log.w(TAG, "signInWithEmail:failure", task.getException());
-                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show();
-                    updateUI(null);
-                    // ...
-                }
+    private void createAccount (String mail, String pwd) {
+        Log.d(TAG, "createAccount:" + mail);
+        if (!validateForm()) {
+            return;
+        }
+        // Für später falls wir ProgressBar noch schaffen beim "hübsch machen"
+        //showProgressBar();
 
-                // ...
-            }
-        });
+        // Create User with Mail
+        mFirebaseAuth.createUserWithEmailAndPassword(mail, pwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //User gets signed in by updating UI
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            //Failing sign in display the following Message
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        // Für später falls wir ProgressBar noch schaffen beim "hübsch machen"
+                        //hideProgressBar();
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        // Für später falls wir ProgressBar noch schaffen beim "hübsch machen"
+        //showProgressBar();
+        mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "singInWithEmail:success");
+                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Log.w(TAG, "singInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void signOut() {
+        mFirebaseAuth.signOut();
+        updateUI(null);
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+        return valid;
+        /*
+        Für später --> herausfinden, wie man anzeigt, dass Info "required" sein soll!
+        String mail = findViewById(R.id.userMail).toString();
+        if (TextUtils.isEmpty(mail)) {
+            //
+        }
+
+         */
     }
 
     private void updateUI(FirebaseUser user) {
+        // Für später falls wir ProgressBar noch schaffen beim "hübsch machen"
+        //hideProgressBar();
         if (user != null) {
-            Toast.makeText(LoginActivity.this, "Logged in",
-                    Toast.LENGTH_SHORT).show();
+            //Toast.makeText(LoginActivity.this, "Logged in",
+            //        Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, JagdeinrichtungenVerwalten.class));
         } else {
-            Toast.makeText(LoginActivity.this, "Des war nix",
+            Toast.makeText(LoginActivity.this, "could not log in",
                     Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText email = findViewById(R.id.userMail);
+        EditText mPwd = findViewById(R.id.userPassword);
+        String mail = email.getText().toString().trim();
+        String pwd = mPwd.getText().toString().trim();
+        int i = v.getId();
+        if (i == R.id.registBtn) {
+            createAccount(mail, pwd);
+            Toast.makeText(LoginActivity.this, mail,
+                    Toast.LENGTH_LONG).show();
+        } if (i == R.id.LoginBtn) {
+            signIn(mail, pwd);
+            Toast.makeText(LoginActivity.this, mail,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
 }
