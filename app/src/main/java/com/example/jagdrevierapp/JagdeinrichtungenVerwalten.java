cc27f49bbd8e19edb8a1e0a2cfe9a53be53b9fd2
StudentM3k1 <michael.schneider2@iubh-fernstudium.de>
2020-08-18 22:01:10 +0200

    Collection in Firebase für Hochsitze erstellt Hochsitze Class in Model angelegt Activite Jagdeinrichtungenverwalten angepasst Jagdeinrichtungen verwalten angepasst --> hängengeblieben an Daten von Collection auslesen

--- app/src/main/java/com/example/jagdrevierapp/JagdeinrichtungenVerwalten.java
+++ app/src/main/java/com/example/jagdrevierapp/JagdeinrichtungenVerwalten.java
@@ -1,40 +1,73 @@
 package com.example.jagdrevierapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import com.example.jagdrevierapp.data.model.Hochsitz;
+
 
 public class JagdeinrichtungenVerwalten extends AppCompatActivity implements View.OnClickListener {
-
-    private static final String TAG = "JagdeinrichtungenVerwalten";
-
     //Const variables
+
+    private static final String TAG = "JagdeinrichtungenVer";
+    private static final String GPS_POSITION = "GPS-Position";
+    private static final String HOCHSITZ_NAME = "HochsitzName";
+    private static final String PASS_KEY = "password";
+
+    private final String COLLECTION_KEY ="HochsitzeMichi";
+
     public static final String ANONYMOUS = "anonymous";
 
     //general variables
     private String mUsername;
     private GoogleSignInClient mSignInClient;
 
+        //View declaration
+        private TextView textAuswahlHochsitze;
+        private TextView textHochsitzName;
+
+
+    //Initialize FireStore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference dbHochsitze = db.collection(COLLECTION_KEY);
+    private DocumentReference docRefHochsitze = dbHochsitze.document();
+
+
     //Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
 
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jagdeinrichtungen_verwalten);
 
-        //onClickListener für Buttons
+        //onClickListener für Buttons for Nav
         Button logoutBtn = findViewById(R.id.logoutBtn);
         logoutBtn.setOnClickListener(this);
         Button mapBtn = findViewById(R.id.mapBtn);
@@ -46,6 +79,26 @@ public class JagdeinrichtungenVerwalten extends AppCompatActivity implements Vie
         Button revierBtn = findViewById(R.id.revierBtn);
         revierBtn.setOnClickListener(this);
 
+
+        //onClickListener für Buttons for Jagdeinrichtungen
+        Button btnStatusHochsitz = findViewById(R.id.btnStatusHochsitz);
+        btnStatusHochsitz.setOnClickListener(this);
+        Button btnBook = findViewById(R.id.btnBook);
+        btnBook.setOnClickListener(this);
+        Button btnDamage = findViewById(R.id.btnDamage);
+        btnDamage.setOnClickListener(this);
+        Button btnInsect = findViewById(R.id.btnInsect);
+        btnInsect.setOnClickListener(this);
+        FloatingActionButton addHochsitz = findViewById(R.id.btnAddHochsitz);
+        addHochsitz.setOnClickListener(this);
+
+        // TextViews to create some texts
+        textAuswahlHochsitze = findViewById(R.id.textAuswahlHochsitze);
+
+        textHochsitzName = findViewById(R.id.textHochsitzName);
+        textHochsitzName.setText("Hochsitz für den Jäger");
+        textAuswahlHochsitze.setText("Alle Hochsitze");
+
         //Initialize Firebase Auth
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
@@ -56,6 +109,22 @@ public class JagdeinrichtungenVerwalten extends AppCompatActivity implements Vie
         } else {
             mUsername = mFirebaseUser.getDisplayName();
         }
+
+        // Read data from firestore
+
+        docRefHochsitze.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    Hochsitz hochsitz = doc.toObject(Hochsitz.class);
+                    textAuswahlHochsitze.setText(doc.get("HochsitzName").toString());
+                }
+            }
+        });
+
+
+
     }
 
     @Override
@@ -86,11 +155,12 @@ public class JagdeinrichtungenVerwalten extends AppCompatActivity implements Vie
         }
         if (i == R.id.mapBtn) {
             startActivity(new Intent(this, RevierKarte.class));
+
         }
         // nur zu testzwecken, da Menübutton etwas mehr in anspruch nehmen wird
         if (i == R.id.revierBtn) {
             startActivity(new Intent(this, Revierverwaltung.class));
-            Toast.makeText(this, "Entfällt wenn Menü ok", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "Entfällt wenn Menü funktioniert");
         }
         if (i == R.id.schussBtn) {
             startActivity(new Intent(this, Schussjournal.class));
@@ -98,6 +168,7 @@ public class JagdeinrichtungenVerwalten extends AppCompatActivity implements Vie
         }
         if (i == R.id.menuBtn) {
             Toast.makeText(this, "Imagine: Menü erscheint", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "Entfällt wenn Menü funktioniert");
         }
     }
 }
