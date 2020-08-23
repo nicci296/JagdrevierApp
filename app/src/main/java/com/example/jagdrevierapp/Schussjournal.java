3017ec673080e773b1feac9b439e6fe9d3b03bc7
nicci296 <61652182+nicci296@users.noreply.github.com>
2020-08-23 13:25:51 +0200

    Erste Gehversuche Schussjournal. User-Klasse mit Code gefüllt. kleine Kosmetikkorrektur in der RegisterActivity.

--- app/src/main/java/com/example/jagdrevierapp/Schussjournal.java
+++ app/src/main/java/com/example/jagdrevierapp/Schussjournal.java
@@ -1,5 +1,8 @@
 package com.example.jagdrevierapp;
 
+import android.util.Log;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +11,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.jagdrevierapp.data.model.Hochsitz;
+import com.example.jagdrevierapp.data.model.User;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.*;
+
+import java.util.Objects;
 
 public class Schussjournal extends AppCompatActivity  {
 
@@ -27,6 +40,14 @@ public class Schussjournal extends AppCompatActivity  {
     //FirebaseVarible von Michi hinzugefügt
     private FirebaseAuth mAuth;
 
+    private final String TAG = "Schussjournal";
+    private final String COLLECTION_KEY = "User";
+
+    //Initialize FireStore
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final CollectionReference dbUser = db.collection(COLLECTION_KEY);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +59,7 @@ public class Schussjournal extends AppCompatActivity  {
         //Initialize Firebase Auth
         //Firebase instance variables
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+        final FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
 
         //Auskommentiert, damit Login-Funktion nicht stört --> Login funktioniert momentan nicht
 
@@ -50,8 +71,6 @@ public class Schussjournal extends AppCompatActivity  {
             //general variables
             String mUsername = mFirebaseUser.getDisplayName();
         }
-
-
 
         //##########################################################
         //###    Buttons from Nav-Header
@@ -84,9 +103,20 @@ public class Schussjournal extends AppCompatActivity  {
             }
         });
 
+        /**
+         * ******************23.08.20 Nico ***************************************************
+         * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+         * TextView soll den aktuellen Nick vom User anzeigen.
+         * Fürs erste wird die Mail-Adresse vom angemeldeten User genommen.
+         *
+         */
+        final TextView userText = findViewById(R.id.user_Name_Jrnl);
+        String showText = mFirebaseUser.getEmail().toUpperCase();
+        int index = showText.indexOf("@");
+        userText.setText(showText.substring(0,index)+"s'");
+
 
     }
-
 
 
 
